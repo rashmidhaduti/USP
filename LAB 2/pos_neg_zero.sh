d39f098a0192efa0f5de57e83a15d96c2d48560c
rashmidhaduti <53811679+rashmidhaduti@users.noreply.github.com>
2020-10-08 20:41:28 +0530

    Add files via upload

---
+++ LAB 2/pos_neg_zero.sh
@@ -0,0 +1,11 @@
+echo "Enter a number" 
+ read num 
+ if [ $num -eq 0 ] 
+ then
+ echo "The number is zero" 
+ elif [ $num -gt 0 ] 
+ then 
+ echo "The number is positive" 
+ else 
+ echo "The number is negetive" 
+ fi 
