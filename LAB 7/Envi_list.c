c33b3e643e964e2a16c8e2f4a97c26e7c5ed26c9
rashmidhaduti <53811679+rashmidhaduti@users.noreply.github.com>
2020-12-28 11:30:09 +0530

    Add files via upload

---
+++ LAB 7/Envi_list.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ #include<unistd.h>
+ int main(int argc,char *argv[])
+ {
+  char **ptr;
+  extern char **environ;
+  for(ptr=environ; *ptr; ptr++)
+   printf("%s\n",*ptr);
+  return 0;
+ }
